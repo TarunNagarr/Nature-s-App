14edba3c8cc454585b06216cfaf1fb5f1fde58b4
Tarun Nagar <tarunnagar8191@gmail.com>
2023-06-14 14:12:38 +0530

    API Filtering, sorting, limiting, Pagination Done.

--- controllers/toursControllers.js
+++ controllers/toursControllers.js
@@ -1,9 +1,67 @@
 const Tour = require('./../models/toursModel');
 
+// COMMON fUNCTION FOR API FUNCTIONALITY
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludeValue = ['sort', 'page', 'limit', 'fields'];
+    excludeValue.forEach(el => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`);
+
+    this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fieldsBy = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fieldsBy);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  pagination() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 // Get All Tours
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // EXECUTE RESPONSE
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+    const tours = await features.query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+const DB =
+  'mongodb+srv://tarun:4Pw7Xf0w0Bdg2p9A@cluster1.kcb3u5s.mongodb.net/?retryWrites=true&w=majority';
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -12,10 +14,9 @@ mongoose
   })
   .then(() => {
     console.log('DB Connect successfully!');
+    // Server
+    const port = process.env.PORT;
+    app.listen(port, () => {
+      console.log(`Server Listing on Port${port}`);
+    });
   });
-
-// Server
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server Listing on Port${port}`);
-});
